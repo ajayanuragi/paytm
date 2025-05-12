86e2ec7ff6a322c36761fdcd292b67de7dbb7f00
Ajay Anuragi <92710715+ajayanuragi@users.noreply.github.com.>
2025-05-12 13:22:08 +0530

    add databse connection and user schema

---
+++ backend/db/db.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { MONGODB_URI } from "../config.js";
+
+async function connectToDB() {
+  try {
+    await mongoose.connect(MONGODB_URI);
+    console.log("✅ Connected to MongoDB");
+    return true;
+  } catch (error) {
+    console.error(`❌ MongoDB connection error: ${error}`);
+    return false;
+  }
+}
+export default connectToDB;
--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,15 @@
 import app from "./app.js";
 import { PORT } from "./config.js";
+import connectToDB from "./db/db.js";
 
-app.listen(PORT, () => {
-  console.log(`backend is running on Port:${PORT}`);
-});
+(async () => {
+  const connected = await connectToDB();
+  if (connected) {
+    app.listen(PORT, () => {
+      console.log(`🚀 Server is running on port ${PORT}`);
+    });
+  } else {
+    console.error("❌ Server not started. MongoDB connection failed.");
+    process.exit(1);
+  }
+})();
