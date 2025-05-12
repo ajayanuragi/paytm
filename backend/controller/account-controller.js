9e517cc726092f19b4d0cf15d6d64b23db17c5c7
Ajay Anuragi <92710715+ajayanuragi@users.noreply.github.com.>
2025-05-12 21:36:51 +0530

    add money transfer

--- backend/controller/account-controller.js
+++ backend/controller/account-controller.js
@@ -1,4 +1,5 @@
-import { findBalance } from "../service/account-service.js";
+import mongoose from "mongoose";
+import { findBalance, transferAmount } from "../service/account-service.js";
 
 export const getBalance = async (req, res) => {
   try {
@@ -19,6 +20,48 @@ export const getBalance = async (req, res) => {
     res.status(500).json({
       success: false,
       message: "Something went wrong",
+      error: error.message,
+    });
+  }
+};
+
+export const transferMoney = async (req, res) => {
+  try {
+    const { to, amount } = req.body;
+    const senderId = req.user.id;
+    if (!to || !amount || amount <= 0) {
+      return res.status(400).json({
+        success: false,
+        message:
+          'Invalid Input: "to" and "amount" are required, and "amount" must be greater tha zero',
+      });
+    }
+    if (!mongoose.Types.ObjectId.isValid(to)) {
+      return res.status(400).json({
+        success: false,
+        message: "Invalid account ID provided.",
+      });
+    }
+
+    const result = await transferAmount(senderId, to, amount);
+
+    res.status(200).json({
+      success: true,
+      message: result.message,
+    });
+  } catch (error) {
+    let errorMessage = "Something went wrong";
+    let statusCode = 500;
+    if (error.message === "Insufficient balance") {
+      errorMessage = error.message;
+      statusCode = 400;
+    } else if (error.message === "Account not found") {
+      errorMessage = "Invalid Account";
+      statusCode = 400;
+    }
+    res.status(statusCode).json({
+      success: false,
+      message: errorMessage,
       error: error.message,
     });
   }
--- backend/service/account-service.js
+++ backend/service/account-service.js
@@ -1,9 +1,28 @@
 import { Account } from "../model/account-model.js";
 
 export const findBalance = async (userId) => {
-  const account = await Account.findOne({userId});
+  const account = await Account.findOne({ userId });
   if (!account) {
     throw new Error("Account not found");
   }
   return account.balance / 100;
+};
+export const transferAmount = async (senderId, recipientId, amount) => {
+  const senderAccount = await Account.findOne({ userId: senderId });
+  if (!senderAccount) {
+    throw new Error("Account not found");
+  }
+  if (senderAccount.balance < amount) {
+    throw new Error("Insufficient balance");
+  }
+  const recipientAccount = await Account.findOne({ userId: recipientId });
+  if (!recipientAccount) {
+    throw new Error("Account not found");
+  }
+  amount = amount * 100;
+  senderAccount.balance -= amount;
+  recipientAccount.balance += amount;
+  await senderAccount.save();
+  await recipientAccount.save();
+  return { message: "Transfer successful" };
 };
