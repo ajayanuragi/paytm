fe5dbf09ce206dd3cad7d531bdbd13ccd162f4dd
Ajay Anuragi <92710715+ajayanuragi@users.noreply.github.com.>
2025-05-12 13:46:32 +0530

    add cors, jsonwebtoken

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,8 @@
 import express from "express";
+import cors from "cors";
 import apiRouter from "./routes/api-router.js";
 const app = express();
 app.use(express.json());
-app.use('/api/v1', apiRouter)
+app.use(cors());
+app.use("/api/v1", apiRouter);
 export default app;
