c5ee9155a366ee2373b22072533352d1228c6117
Ajay Anuragi <92710715+ajayanuragi@users.noreply.github.com.>
2025-05-12 13:36:49 +0530

    add router

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 import express from "express";
+import apiRouter from "./routes/api-router.js";
 const app = express();
 app.use(express.json());
-
+app.use('/api/v1', apiRouter)
 export default app;
