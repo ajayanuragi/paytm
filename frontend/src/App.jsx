190ced2d01f46978be308d65c2927a9b3370f671
Ajay Anuragi <92710715+ajayanuragi@users.noreply.github.com.>
2025-05-13 07:43:54 +0530

    add routing

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,19 @@
+import { Route, Router } from "react-router";
+import { Signup } from "./pages/Signup";
+import { Signin } from "./pages/Signin";
+import { Dashboard } from "./pages/Dashboard";
+import { SendMoney } from "./pages/SendMoney";
 function App() {
-  return <div className="text-3xl min-h-screen flex items-center justify-center">Paytm</div>;
+  return (
+    <>
+      <Router>
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/signin" element={<Signin />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/send" element={<SendMoney />} />
+      </Router>
+    </>
+  );
 }
 
 export default App;
